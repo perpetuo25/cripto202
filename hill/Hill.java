c4a60417c5579a96db991c98a4ac5f5626b447ae
perpetuo25 <lince97@ciencias.unam.mx>
2020-03-18 03:36:49 -0600

    Proyecto listo

--- hill/Hill.java
+++ hill/Hill.java
@@ -12,31 +12,63 @@ package hill;
  *  @author Eric Sanchez Sarmiento, Fernando Altamirano, Tania 
  */
 public class Hill {
-    
+    /**
+     * Metodo para encriptar un mensaje con clave de longitud 9.
+     * @param texto
+     * @param llave
+     * @return el mensaje cifrado
+     */
     public static String encrip3x3(String texto, String llave){
         String cifrado;
         int[] keyArr = Alphabet.textToIndex(llave);
         int[] text = Alphabet.textToIndex(texto);
         int div = text.length /3;
         int [][] clave = matrices.matrix(keyArr,3,3);
+        //System.out.println("La matriz Clave es:"); //Quitar comentarios para imprimir matrices paso por paso
+        //matrices.imprimeMatriz(clave);
         int [][] mensaje = matrices.matrix(text,div,3);
-            cifrado = matrices.getCifrado(matrices.multiplicaMatrices(clave, mensaje));
+        //System.out.println("La matriz del mensaje es:); //Quitar comentarios para imprimir matrices paso por paso
+        //matrices.imprimeMatriz(mensaje);
+            cifrado = matrices.getCifrado2x2(matrices.multiplicaMatrices(clave, mensaje));
             return cifrado;
             
         }
+    /**
+     * Metodo para encriptar un mensaje con clave de longitud 4.
+     * @param texto
+     * @param llave
+     * @return el mensaje cifrado
+     */
     public static String encrip2x2(String texto, String llave){
         String cifrado;
         int[] keyArr = Alphabet.textToIndex(llave);
         int[] text = Alphabet.textToIndex(texto);
         int div = text.length /2;
         int [][] clave = matrices.matrix(keyArr,2,2);
+        //System.out.println("La matriz Clave es:"); //Quitar comentarios para imprimir matrices paso por paso
+        //matrices.imprimeMatriz(clave); //Quitar comentarios para imprimir matrices paso por paso
+        double deter = matrices.determinante2x2(clave);
         int [][] mensaje = matrices.matrix(text,div,2);
-            cifrado = matrices.getCifrado(matrices.multiplicaMatrices(clave, mensaje));
+        //System.out.println("La matriz del mensaje es:); //Quitar comentarios para imprimir matrices paso por paso
+        //matrices.imprimeMatriz(mensaje); //Quitar comentarios para imprimir matrices paso por paso
+        if(matrices.MCD((int)deter, 27) ==1){
+            cifrado = matrices.getCifrado2x2(matrices.multiplicaMatrices(clave, mensaje));
             return cifrado;
-            
         }
+        return "La matriz no es invertible, no se podrá hacer el desencriptado";
+        }
+    
+    
+    /**
+     * Metodo para encriptar un mensaje, manda a llamar a cualquiera de los metodos de encriptación dependiendo de la longitid 
+     * de su clave.
+     * @param texto
+     * @param llave
+     * @return el mensaje cifrado
+     */
     public static String encriptaHill(String texto, String llave){
         int[] keyArr = Alphabet.textToIndex(llave);
+        
         if(keyArr.length == 9){
             return encrip3x3(texto,llave);
         }else{
@@ -45,42 +77,109 @@ public class Hill {
     }
         return "La clave debe ser de tamaño 4 o 9";
     }
-    
-   
+    /**
+     * Metodo para descencriptar un mensaje con clave de longitud 4.
+     * @param texto
+     * @param llave
+     * @return el mensaje descifrado
+     */
+    public static String desencripta2x2(String texto, String llave){
+        int[] keyText = Alphabet.textToIndex(llave);
+        int[] text = Alphabet.textToIndex(texto);
+        int div = text.length /2;
+        int[][] matll = matrices.matrix(keyText, 2, 2);
+        int[][] inver = matrices.inversa2x2(matll);
+        //System.out.println("La matriz inversa de la clave es:); //Quitar comentarios para imprimir matrices paso por paso
+        //matrices.imprimeMatriz(inver); //Quitar comentarios para imprimir matrices paso por paso
+        int[][] textM = matrices.matrix(text, div, 2);
+        //System.out.println("La matriz del mensaje es:); //Quitar comentarios para imprimir matrices paso por paso
+        //matrices.imprimeMatriz(textM); //Quitar comentarios para imprimir matrices paso por paso
+        int[][] mult = matrices.multiplicaMatrices(inver, textM);
+        //System.out.println("La matriz resultante de la multiplicación es:); //Quitar comentarios para imprimir matrices paso por paso
+        //matrices.imprimeMatriz(mult); //Quitar comentarios para imprimir matrices paso por paso
+        String mensaje = matrices.getCifrado2x2(mult);
+        return mensaje;
+        
+    }
+     /**
+     * Metodo para descencriptar un mensaje con clave de longitud 9.
+     * @param texto
+     * @param llave
+     * @return el mensaje descifrado
+     */
             
-    
+    public static String desencripta3x3(String texto, String llave){
+        String cifrado;
+        int[] keyArr = Alphabet.textToIndex(llave);
+        int[] text = Alphabet.textToIndex(texto);
+        int div = text.length /3;
+        int [][] claveInv =matrices.inversa3x3(matrices.matrix(keyArr,3,3));
+        //System.out.println("La matriz inversa de la clave es:); //Quitar comentarios para imprimir matrices paso por paso
+        //matrices.imprimeMatriz(claveInv); //Quitar comentarios para imprimir matrices paso por paso
+        int [][] mensaje = matrices.matrix(text,div,3);
+        //System.out.println("La matriz del mensaje es:); //Quitar comentarios para imprimir matrices paso por paso
+        //matrices.imprimeMatriz(mensaje); //Quitar comentarios para imprimir matrices paso por paso
+            cifrado = matrices.getCifrado2x2(matrices.multiplicaMatrices(claveInv, mensaje));
+            return cifrado;
+            
+        }
+      /**
+     * Metodo para descencriptar un mensaje, manda a llamar a los metodos de descencriptación
+     * segun sea el tamaño de la clave.
+     * @param texto
+     * @param llave
+     * @return el mensaje descifrado
+     */
+        public static String desencriptaHill (String texto, String llave){
+            if(llave.length()==9){
+                return desencripta3x3(texto,llave);
+            }else{
+                return desencripta2x2(texto, llave);
+            }
+            
+        }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        /*
-       int arreglo[] = {2,15,13,19,21,11};
-       int key[]= {5,15,18,20,0,11,4,26,0};
-       matrices matr = new matrices();
-      int [][] ene = matr.matrix(arreglo,2,3);
-      System.out.println("clave");
-      int[][] keys = matr.matrix(key,3,3);
+        /**
+         * Aquí se imprimen los mensajes
+         */
       
-      int[][] cif = matr.multiplicaMatrices(keys,ene);
-      System.out.println(getCifrado(cif));
+      System.out.println("Encriptado de 2x2");
+      System.out.println("La Clave es: FEDW\nLa palabra a cifrar es: CONSUL");
+      String ciff2x2 = encriptaHill("CONSUL","FEDW"); // AQUI SE LLAMA A LOS METODOS
+      System.out.println("La palabra encriptada es: "+ciff2x2+"\n");
+      System.out.println("Desencriptado de 2x2");
+      System.out.println("La Clave es: FEDW\nLa palabra a desifrar es: PAGQÑS");
+      String deciff2x2 = desencriptaHill("PAGQÑS","FEDW"); // AQUI TAMBIEN 
+      System.out.println("La palabra desencriptada es: "+deciff2x2+"\n");
+      
+      
+      System.out.println("Ecriptado 3x3");
+      System.out.println("La palabra a cifrar es: CONSUL");
+      System.out.println("La clave es: FORTALEZA");
+      String ciff3x3 = encriptaHill("CONSUL","FORTALEZA");// AQUI 
+      System.out.println("La palabra encriptada es "+ciff3x3+"\n");
+      System.out.println("Desencriptado 3x3");
+      System.out.println("La palabra a descifrar es: KUTÑOB");
+      System.out.println("La clave es: FORTALEZA");
+      String deciff3x3 =desencripta3x3("KUTÑOB","FORTALEZA");// Y AQUI
+      System.out.println("La palabra descencriptada es "+deciff3x3+"\n");
+      
+      
+      
+      
+      
+      
+      
+      
      
-      String ciff = encriptaHill("CONSULTAR","FORTALEZA");
-      String ciff2 = encriptaHill("CONSUL","HOLA");
-      System.out.println(ciff);
-       System.out.println(ciff2);
-    
-      int[] keyArr = Alphabet.textToIndex("HOLA");
-      double[][] mult = matrices.inversa2x2(matrices.matrix(keyArr, 2, 2));
       
-      int [][] mensaje = matrices.matrix(Alphabet.textToIndex("WVYIOO"),3,2);
-      double[][] rest = matrices.multiplicaMatricesdo(mult, mensaje);
-      matrices.imprimeMatriz(rest);
-     */
-        double x = -143;
-        double y = 4295;
-        double mod = (x/y) % 27;     
-        System.out.println(mod);
+      
+     
+       
       
        
     }
--- hill/matrices.java
+++ hill/matrices.java
@@ -27,12 +27,37 @@ public class matrices {
          for(int i = 0; i < M; i++){
              numeros[j][i] = arr[contador];
              contador++;
-             System.out.print("[" + numeros[j][i] + "]"); //descomentar para ver la matriz resultante
+             //System.out.print("[" + numeros[j][i] + "]"); //descomentar para ver la matriz resultante
           }
-        System.out.println(""); //descomentar para ver la matriz resultante
+        //System.out.println(""); //descomentar para ver la matriz resultante
        }
              return numeros;
      }
+   
+   /**
+    * Metodo auxiliar para calcular el modulo de un numero, este metodo es de apoyo para numeros negativos
+    * @param number
+    * @return el modulo del numero.
+    */
+   public static int modulo (int number){
+      if (number < 0){
+         int modul = (number % 27) + 27;
+         return modul;
+      } 
+     return number%27;
+       
+       
+   }
+   
+   public static double modulod (double number){
+      if (number < 0){
+         double modul = (number % 27) + 27;
+         return modul;
+      } 
+     return number%27;
+       
+       
+   }
    /**
     * Metodo para multiplicar matrices
     * @param matrizClave la matriz con la clave, esta debe ser de NxN
@@ -48,7 +73,7 @@ public class matrices {
                 for(int j=0; j<matrizMensaje[0].length; j++){
                     a = a + (matrizClave[i][j]*matrizMensaje[h][j]);
                 }
-                mult[h][i] = a % 27; // se saca el modulo 27 de cada elemento de la matriz.
+                mult[h][i] = modulo(a) ; // se saca el modulo 27 de cada elemento de la matriz.
             }
         }
 
@@ -59,7 +84,7 @@ public class matrices {
     * @param matrizCifrada
     * @return una cadena cifrada
     */
-    public static String getCifrado(int[][] matrizCifrada){
+    public static String getCifrado2x2(int[][] matrizCifrada){
         String mesCif = "";
         for(int i=0; i<matrizCifrada.length; i++){
             for(int j=0; j<matrizCifrada[i].length; j++){
@@ -69,15 +94,31 @@ public class matrices {
 
         return mesCif;
     }
-    
+    /**
+     * Método para calcular el inverso modular de un numero
+     * @param a
+     * @param b
+     * @return i si el numero tiene inverso, -1 en caso contrario
+     */
+     public static int findInverseMult(int a, int b){
+        for (int i = 0; i<b; i++){
+            if(((i * a) % b) == 1)
+                return i;
+         //System.out.print(i*a+"  ");
+    }
+        return -1;
+}
     /**
      * Metodo para obtener el determinante de una matriz de 2x2
      * @param matriz la matriz de la que obtenemos el determinante
      * @return 
      */
-    public static double determinante2x2(int[][] matriz ){
-        double deter=matriz[0][0]*matriz[1][1]-matriz[0][1]*matriz[1][0];       
-        return  deter;
+    public static int determinante2x2(int[][] matriz ){
+        int deter=(matriz[0][0]*matriz[1][1])-(matriz[0][1]*matriz[1][0]);
+        if(deter <0)
+            return (deter % 27) + 27;
+        
+        return  deter % 27;
     }
     /**
      * Metodo para calcular la adjunta de una matriz de 2x2
@@ -90,45 +131,41 @@ public class matrices {
        adj[1][1] = matriz[0][0];
        adj[1][0] = matriz[1][0]*-1;
        adj[0][1] = matriz[0][1]*-1;
+       //System.out.println("");
+       //imprimeMatriz(adj);
        return adj;
     }  
-    
-    public static double[][] inversa2x2 (int[][]matriz){
-         double divDet = 1/determinante2x2(matriz);
-         double [][] inv = new double[2][2];
+    /**
+     * Metodo para generar la matriz inversa de otra matriz
+     * @param matriz
+     * @return 
+     */
+    public static int[][] inversa2x2 (int[][]matriz){
+       
+         int determod = determinante2x2(matriz);
+         int inversoMult = findInverseMult(determod,27);
+         //System.out.println("");
+         //System.out.println(inversoMult);
+         int [][] inv = new int[2][2];
          int[][]adj= adjunta2x2(matriz);
-         inv[0][0]= adj[0][0]*divDet;
-         inv[0][1]= adj[0][1]*divDet;
-         inv[1][0]= adj[1][0]*divDet;
-         inv[1][1]= adj[1][1]*divDet;
-         for(int j = 0; j < 2; j++){
-            for(int i = 0; i < 2; i++){
-                System.out.print("[" + inv[j][i] + "]");
-            }
-           System.out.println(""); 
-         }
+         inv[0][0]= modulo(adj[0][0]*inversoMult);
+         inv[0][1]= modulo(adj[0][1]*inversoMult);
+         inv[1][0]= modulo(adj[1][0]*inversoMult);
+         inv[1][1]= modulo(adj[1][1]*inversoMult);
+         //for(int j = 0; j < 2; j++){
+           // for(int i = 0; i < 2; i++){
+             //   System.out.print("[" + inv[j][i] + "]"); // si quieres ver como se imprime la matriz descomenta estas lineas
+            //}
+           //System.out.println(""); 
+         //}
          return inv;
     
     }
-     public static double[][] multiplicaMatricesdo(double[][] matrizClave, int[][] matrizMensaje){
-        double[][] mult = new double[matrizMensaje.length][matrizMensaje[0].length];
-        
-        for(int h=0; h<mult.length;h++){
-            for(int i=0; i<matrizClave.length; i++){ 
-                double a = 0;
-                for(int j=0; j<matrizMensaje[0].length; j++){
-                    a = a + (matrizClave[i][j]*matrizMensaje[h][j]);
-                    
-                }
-                mult[h][i] = a; // se saca el modulo 27 de cada elemento de la matriz.
-              
-            }
-            
-        }
-
-        return mult;
-    }
-     public static void imprimeMatriz(double[][]matriz){
+   /**
+    * Método auxiliar que nos imprime una matriz indicada
+    * @param matriz 
+    */
+     public static void imprimeMatriz(int[][]matriz){
          for(int j = 0; j < matriz.length; j++){
             for(int i = 0; i < 2; i++){
                 System.out.print("[" + matriz[j][i] + "]");
@@ -137,6 +174,76 @@ public class matrices {
          }
      
      }
+   
+    /**
+     * Metodo auxiliar para calcular el maximo comun divisor de 2 números
+     * @param a
+     * @param b
+     * @return el mcd de a y b
+     */
+    public static int MCD (int a, int b) {
+        int mcd = 0;
+	int r = 0;
+	a = Math.abs (a); //Tomamos el valor absoluto
+	b = Math.abs (b);
+	while (true){
+            if (b == 0){
+                mcd = a;
+		break;
+            }else {
+		r = a % b;
+		a = b;
+		b = r;
+            }
+        }
+        return mcd;
+
+    }
+    /**
+     * Metodo para obtener la inversa de una matriz de 3x3 
+     * @param B
+     * @return la matriz inversa en mod 27
+     */
+    public static int[][] inversa3x3(int[][]B){
+        int [][]C=new int [3][3];
+        int dete,x00,x01,x02,x10,x11,x12,x20,x21,x22;
+        dete=((B[0][0]*((B[1][1]*B[2][2])-(B[1][2]*B[2][1])))-(B[0][1]*((B[1][0]*B[2][2])-(B[2][0]*B[1][2])))+(B[0][2]*((B[1][0]*B[2][1])-(B[2][0]*B[1][1]))));
+        //System.out.println(dete);
+        int determinante = findInverseMult((int)dete,27); 
+        //System.out.println(determinante);
+        if(determinante!=0){
+         x00=(((B[1][1]* B[2][2] - B[2][1]* B[1][2]))*determinante);
+         x01=((-(B[1][0]* B[2][2] - B[2][0]* B[1][2]))*determinante);
+         x02=((B[1][0]* B[2][1] - B[2][0]* B[1][1]))*determinante;
+         x10=(-(B[0][1]* B[2][2] - B[2][1]* B[0][2]))*determinante;
+         x11=((B[0][0]* B[2][2] - B[2][0]* B[0][2]))*determinante;
+         x12=(-(B[0][0]* B[2][1] - B[2][0]* B[0][1]))*determinante;
+         x20=((B[0][1]* B[1][2] - B[1][1]* B[0][2]))*determinante;
+         x21=(-(B[0][0]* B[1][2] - B[1][0]* B[0][2]))*determinante;
+         x22=((B[0][0]* B[1][1] - B[1][0]* B[0][1] ))*determinante;
+            C[0][0]=modulo((int)x00);
+            C[0][1]=modulo((int)x10);
+            C[0][2]=modulo((int)x20);
+            C[1][0]=modulo((int)x01);
+            C[1][1]=modulo((int)x11);
+            C[1][2]=modulo((int)x21);
+            C[2][0]=modulo((int)x02);
+            C[2][1]=modulo((int)x12);
+            C[2][2]=modulo((int)x22);
+             for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    //System.out.print("|"+ C[i][j] +"|");
+                }
+                //System.out.println();
+            } 
+            return C;
+            
+      
+            }else{
+                return null;
+        }
+    }
+
      
    }
   
